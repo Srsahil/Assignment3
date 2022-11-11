f230d8912938f10750f02a451dc987db45771d38
sahil sonkar <88874387+Srsahil@users.noreply.github.com>
2022-11-12 01:28:44 +0530

    Update Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -1,26 +1,26 @@
-package listprogram;                                                                                                       
-import java.util.*;                                                                                                        
-public class Mobile{                                                                                                       
-public static void main(String args[]){                                                                                    
- //Create a List                                                                                                           
- List<String> list=new ArrayList<String>();                                                                                
- //Add elements in the List                                                                                                
-                                                                                                                           
-                                                                                                                           
- list.add("Web Automation");                                                                                               
- list.add("API Automation");                                                                                               
- list.add("Mobile Automation");                                                                                            
-                                                                                                                           
-                                                                                                                           
- //iterating for loop to print the element in the list                                                                     
-if(list.contains("Mobile "))                                                                                               
-{                                                                                                                          
-     System.out.println("True");                                                                                                                      
-                                                                                                                           
-}                                                                                                                          
-else    
-{
-	System.out.println("False");
-}
-}
-}
+package listprogram;                                                                                                                                                                 
+import java.util.*;                                                                                                                                                                  
+public class Mobile{                                                                                                                                                                 
+public static void main(String args[]){                                                                                                                                              
+ //Create a List                                                                                                                                                                     
+ LinkedList<String> list=new LinkedList <String>();                                                                                                                                  
+ //Add elements in the List                                                                                                                                                          
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+ list.add("Web Automation");                                                                                                                                                         
+ list.add("API Automation");                                                                                                                                                         
+ list.add("Mobile Automation");                                                                                                                                                      
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+ //iterating for loop to print the element in the list                                                                                                                               
+        boolean res = list.stream().anyMatch((a) -> a.startsWith("Mobile"));                                                                                                         
+             System.out.println("Do any string begins with letter Mobile = "+res);                                                                                                   
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+                                                                                                                                                                                     
+}                                                                                                                                                                                    
+}                                                                                                                                                                                    
